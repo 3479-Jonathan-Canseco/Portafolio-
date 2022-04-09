870e76f3fc455c1ebc50a35c94be0468c6346da8
3479-Jonathan-Canseco <101839831+3479-Jonathan-Canseco@users.noreply.github.com>
2022-04-09 03:12:08 -0500

    Add files via upload

---
+++ app/vistas/Home/Home.php
@@ -0,0 +1,27 @@
+<!-- Bloques de texto
+            Parrafos
+            Saltos de linea
+            Divisiones
+            Texto con formato
+            Alineacion de textos
+         -->
+         <!-- Parrafos -->
+         <p>
+         Avance <br>
+         De <br>
+         Portafolio <br>  
+         Web </p>
+        <p>
+           
+           <h1>3479 Jonathan Osvaldo Canseco Martinez</h1>
+           <h2>Portafolio</h2>
+           <h3>Mui buenas</h3>
+           <h4></h4>
+           <h5></h5>
+           <h6>Avance</h6>
+           <h7>Pagina Web</h7>
+        </p>
+
+        <div>
+
+        </div>
---
+++ app/vistas/main/foot.php
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="sys/libs/js/bootstrap.js"></script>
+<div class="titulo1"> 
+    <footer>Crando una pag web</footer>
+</div>
+</body>  
+</html>
