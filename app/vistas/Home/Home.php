37c1cff7afadd9f4ca07f1c552522b988f760758
3479-Jonathan-Canseco <101839831+3479-Jonathan-Canseco@users.noreply.github.com>
2022-04-20 11:42:54 -0500

    Update Home.php

--- app/vistas/Home/Home.php
+++ app/vistas/Home/Home.php
@@ -13,7 +13,7 @@
            <b> 
            </b>
            <h1>3479 Jonathan Osvaldo Canseco Martinez</h1>
-           <h2>Ingeniero en Progrmamacion</h2>
+           <h2>Ingeniero en Programacion</h2>
            <h3>tecnico en Base de Datos</h3>
            <h3>Tecnico en Programacion</h3>
            <body background="sys/libs/images/programacion.JPG">
